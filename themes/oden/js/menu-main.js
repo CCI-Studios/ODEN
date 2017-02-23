825daea699141141cab2e5fddd8615eec7d42bce
Chris Morris <cmorris@ccistudios.com>
2017-02-23 10:00:27 -0500

    responsive changes for the header/footer and homepage

--- themes/oden/js/menu-main.js
+++ themes/oden/js/menu-main.js
@@ -1,6 +1,6 @@
 (function(){
-  document.querySelector(".menu--main").addEventListener("click", openDropdown)
-  
+  //main menu dropdowns
+  document.querySelector(".menu--main").addEventListener("click", openDropdown);
   function openDropdown(e) {
     if (e.target.tagName.toLowerCase() != "a") return;
     if (!e.target.parentNode.classList.contains("menu-item--expanded")) return;
@@ -10,5 +10,12 @@
       open.classList.remove("open");
     }
     e.target.parentNode.classList.toggle("open");
+  }
+  
+  //mobile menu button
+  document.querySelector(".menu-button").addEventListener("click", openMenu);
+  function openMenu(e) {
+    e.preventDefault();
+    document.querySelector("body").classList.toggle("menu-open");
   }
 })();
