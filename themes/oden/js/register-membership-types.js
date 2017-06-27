de15eb804a816b705900bbb3c0936265fe13c3d3
Chris Morris <cmorris@ccistudios.com>
2017-06-27 15:57:21 -0400

    show descriptions for each membership type in the register form close #78

---
+++ themes/oden/js/register-membership-types.js
@@ -0,0 +1,28 @@
+(function(){
+  var container = document.getElementById("edit-field-membership-type-wrapper");
+  if (!container) return;
+  var labels = container.querySelectorAll("label.option");
+  for (var i=0; i<labels.length; i++) {
+    labels[i].innerHTML += "<span class='membership-helper' tabindex='0'>?</span>";
+  }
+  
+  var descriptions = container.querySelectorAll(".description > span");
+  hideAllDescriptions();
+  
+  var buttons = container.querySelectorAll(".membership-helper");
+  for (var i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener("focus", (function(i){return function(e){showDescription(e, i);}})(i));
+    buttons[i].addEventListener("blur", (function(i){return function(e){hideDescription(e, i);}})(i));
+    buttons[i].addEventListener("click", function(e){e.stopPropagation();e.preventDefault();});
+  }
+  
+  function showDescription(e, i) {
+    hideAllDescriptions();
+    descriptions[i].classList.remove("hide");
+  }
+  function hideAllDescriptions() {
+    for (var i=0; i<descriptions.length; i++) {
+      descriptions[i].classList.add("hide");
+    }
+  }
+})();
